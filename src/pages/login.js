1b6d17edd9424e1051ab35a5fd454a6bcb1d34ca
shaik sandeep <shaiksandeep999@gmail.com>
2025-09-13 12:40:36 +0530

    changed

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,7 +20,7 @@ export default function Login() {
               <label>
                 <input type="checkbox" /> Remember Me
               </label>
-              <a href="/#">Forget Password</a>
+              {/* <a href="/#">Forget Password</a> */}
               <Link to="/forgot-password">Forget Password</Link>
             </div>
 
